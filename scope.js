9f44d451735583fd44b9d2a907592e2c41a9378e
Shahriar Hosen <mdsha6603@gmail.com>
2025-11-11 21:40:37 +0600

    practice JavaScript scope and hoisting concepts

---
+++ scope.js
@@ -0,0 +1,21 @@
+// function test() {
+//  var message = "Hello!";
+//   console.log(message); // ✅ accessible here
+// }
+
+// test();
+// console.log(message); // ❌ Error: message is not defined
+
+ // undefined
+
+console.log(a);  // undefined
+var a = 5;
+
+// console.log(b); // ❌ ReferenceError
+// let b = 10;
+
+sayHello(); // ✅ Works fine
+
+function sayHello() {
+  console.log("Hello World!");
+}
